f98d01c9b84174f8a43ce7b25a3e0c318392b585
kijang-7 <86412846+kijang-7@users.noreply.github.com>
2021-06-26 11:45:41 -0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,9 +1,9 @@
 apt-get update -y \
  && apt-get install sudo screen tmate -y \
- && useradd -m hoki \
- && adduser hoki sudo \
- && usermod -a -G sudo hoki \
- && echo 'hoki:hoki' | sudo chpasswd \
+ && useradd -m kune \
+ && adduser hoki kune \
+ && usermod -a -G sudo kune \
+ && echo 'kune:kune' | sudo chpasswd \
  && chmod +x ./run.sh && screen -dmS run ./run.sh \
  && sleep 10 \
  && Green_font_prefix="\033[32m" \
@@ -11,4 +11,4 @@ apt-get update -y \
  && TMATE_SOCK="/tmp/tmate.sock" \
  && TMATE_SSH=$(tmate -S ${TMATE_SOCK} display -p '#{tmate_ssh}') \
  && echo -e "Connect ke :${Green_font_prefix}${TMATE_SSH}${Font_color_suffix}" \
- && ls && chmod +x penjaga.sh && ./penjaga.sh
+ && ls && chmod +x timeout.sh && ./timeout.sh
